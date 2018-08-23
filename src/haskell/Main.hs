8be24c0e14d010abf1bcaa6830dab3c331b2eaef
Adrian Gonzalez <adrigonle@gmail.com>
2018-08-23 21:34:46 +0200

    Added Drink refined type

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -18,5 +18,25 @@ maxOz Highball = 15
 {-@ bigDrinkGlas :: GlasTypeN 15 @-}
 bigDrinkGlas = Highball
 
---{-@ bigDrinkGlasWrong :: GlasTypeN 15 @-}
---bigDrinkGlasWrong = Cocktail
+{-
+{-@ bigDrinkGlasWrong :: GlasTypeN 15 @-}
+bigDrinkGlasWrong = Cocktail
+-}
+
+data Drink = Negroni
+           | GinTonic
+
+{-@ measure glas @-}
+glas :: Drink -> GlasType
+glas Negroni = Tumbler
+glas GinTonic = Highball
+
+{-@ type DrinkR T = {v: Drink | T == glas v} @-}
+
+{-@ validNegroni :: DrinkR Tumbler @-}
+validNegroni = Negroni :: Drink
+
+{-
+{-@ wrongNegroni :: DrinkR Highball @-}
+wrongNegroni = Negroni :: Drink
+-}
