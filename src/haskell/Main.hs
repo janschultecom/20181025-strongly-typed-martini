e1459e08f0ab4535577cdaf6c9cc4e584d2f2504
Jan Schulte <jan@janschulte.com>
2018-09-18 12:54:00 +0200

    Add pouring draft

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -2,9 +2,6 @@
 
 module Main where
 
-main :: IO ()
-main = putStrLn "Refinement types"
-
 data GlasType = Tumbler
               | Cocktail
               | Highball
@@ -47,42 +44,60 @@ data Ingredient = Rum
 
 data Ounce = Oz Ingredient
 
-data ShakerType = Boston
+data ShakerType = Boston | French
 
 {-@ measure shakerTypeOz @-}
 shakerTypeOz :: ShakerType -> Int
-shakerTypeOz Boston = 2
+shakerTypeOz Boston = 28
+shakerTypeOz French = 20
 
 {-@ type ShakerTypeN N = {v: ShakerType | N == shakerTypeOz v} @-}
 
-{-@ validBoston :: ShakerTypeN 2 @-}
+{-@ validBoston :: ShakerTypeN 28 @-}
 validBoston = Boston
 
 {-
-{-@ nonValidBoston :: ShakerTypeN 5 @-}
+{-@ nonValidBoston :: ShakerTypeN 29 @-}
 nonValidBoston = Boston
 -}
 
 data Shaker = Empty
             | Mix Int Ounce Shaker
 
-{-@ measure size @-}
-size :: Shaker -> Int
-size Empty = 0
-size (Mix amount oz s) = size s + amount
+{-@ measure volume @-}
+volume :: Shaker -> Int
+volume Empty = 0
+volume (Mix amount oz s) = volume s + amount
 
-{-@ type ShakerN ST = {v: Shaker | size v <= shakerTypeOz ST } @-}
+{-@ type ShakerN ST = {v: Shaker | volume v <= shakerTypeOz ST } @-}
 
 {-@ shaker0 :: ShakerN Boston @-}
 shaker0 = Empty
 
 {-@ shaker1 :: ShakerN Boston @-}
-shaker1 = Mix 1 (Oz Rum) Empty
+shaker1 = Mix 10 (Oz Rum) Empty
 
 {-@ shaker2 :: ShakerN Boston @-}
-shaker2 = Mix 1 (Oz Rum) (Mix 1 (Oz Rum) Empty)
+shaker2 = Mix 20 (Oz Rum) (Mix 7 (Oz Rum) Empty)
 
 {-
 {-@ shaker3 :: ShakerN Boston @-}
 shaker3 = Mix (Oz Rum) (Mix (Oz Rum) (Mix (Oz Rum) Empty)) -- will fail
 -}
+
+data Glas = Glas GlasType
+
+{-@ pour ::  s: Shaker -> x : { gt:GlasType | volume s <= maxOz gt } -> Glas @-}
+pour :: Shaker -> GlasType -> Glas
+pour shaker = Glas
+
+
+validGlas :: Glas 
+validGlas = pour shaker1 Highball 
+
+main :: IO ()
+main = do 
+    _ <- print "Volume shaker1 " *> print (volume shaker1)
+    _ <- print "maxOz Highball " *> print (maxOz Highball)
+    _ <- print "volume s <= maxOz gt " *> print (volume shaker1 <= maxOz Highball )
+    pure ()
